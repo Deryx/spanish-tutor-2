6ca7b45deb38e994710b93ca0cb9634e8efd29fa
Deryx <deryxescott@gmail.com>
2019-04-29 22:03:38 -0400

    Update home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -21,5 +21,40 @@ export class HomeComponent {
       header: 'letter pronunciation',
       summary: ''
     }
+    this.verbFlashcardCard = {
+      image: '',
+      header: 'verb flashcard',
+      summary: 'Before you dive into the Verb Conjugator, study over 100 most commonly used spanish verbs with the Verb Flashcard.'
+    }
+    this.vocabularyFlashcardCard = {
+      image: '',
+      header: 'vocabulary flashcard',
+      summary: 'Want to study spanish vocabulary before you take the Vocabulary Quiz or play the Vocabulary Slider, Vocabulary Scramble or Vocabulary Completion games? Study up with the Vocabulary Flashcard'
+    }
+    this.verbConjugatorCard = {
+      image: '',
+      header: 'verb conjugator',
+      summary: 'Studied the most commonly used spanish verbs with the Verb Flashcard and want to test your skills? Well, test your skills with the Verb Conjugator!'
+    }
+    this.vocabularyCompletionCard = {
+      image: '',
+      header: 'vocabulary Completion',
+      summary: 'Vocabulary Scramble too easy because all of the letters are there? Try your luck with Vocabulary Completion!'
+    }
+    this.vocabularyQuizCard = {
+      image: '',
+      header: 'vocabulary quiz',
+      summary: 'Studied the Vocabulary Flashcards and want to perform a standard test of your spanish vocabulary skills? Go to the Vocabulary Quiz!'
+    }
+    this.vocabularyScrambleCard = {
+      image: '',
+      header: 'vocabulary scramble',
+      summary: 'Think you have a handle on your spanish vocabulary and want to test your ability to spell the words? Then try the Vocabulary Scramble!'
+    }
+    this.vocabularySliderCard = {
+      image: '',
+      header: 'vocabulary slider',
+      summary: 'Want to test your knowledge of spanish vocabulary by matching spanish words with their corresponding translation? Then play with the Vocabulary Slider.'
+    }
    }
 }
