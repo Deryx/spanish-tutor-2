1527dc256d9b1246f5942d582cdd0409ed3767c6
Deryx <deryxescott@gmail.com>
2021-03-22 19:43:54 -0400

    update docs and conjugator report

--- src/app/components/conjugator-report/conjugator-report.component.ts
+++ src/app/components/conjugator-report/conjugator-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input, Inject, OnInit } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { Router } from "@angular/router";
 
@@ -7,8 +7,17 @@ import { Router } from "@angular/router";
   templateUrl: './conjugator-report.component.html',
   styleUrls: ['./conjugator-report.component.css']
 })
-export class ConjugatorReportComponent {
+export class ConjugatorReportComponent implements OnInit {
   @Input() reportInfo: any;
+
+  ngOnInit() {
+    const longOverlay: any = document.querySelector( '#long-overlay' );
+    const report: any = document.querySelector( 'section > div' );
+
+    if(longOverlay) {
+      report.style.marginTop = "50rem";
+    } 
+  }
 
   constructor( @Inject(DOCUMENT) private document: Document, private router: Router ) {}
 
