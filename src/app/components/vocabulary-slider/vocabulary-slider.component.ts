25928b1b4a4a5f54e357ec9daa6ff0ecbef943d9
Deryx <deryxescott@gmail.com>
2021-04-09 21:55:28 -0400

    Update docs and vocabulary slider component

--- src/app/components/vocabulary-slider/vocabulary-slider.component.ts
+++ src/app/components/vocabulary-slider/vocabulary-slider.component.ts
@@ -75,7 +75,7 @@ export class VocabularySliderComponent {
           this.dictionary = categoryDictionary;
         }
 
-        this.getQuestionSet( numberCards, numberCards, this.dictionary.length );
+        this.getQuestionSet( this.numberSlides, numberCards, this.dictionary.length );
         this.displaySlideSet( this.currentSlideSet );;
       }, (error) => {
         console.log('there was an error sending the query', error);
