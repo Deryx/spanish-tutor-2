29d3a5f77ca21205c0890ebfe017c83731235c92
Deryx <deryxescott@gmail.com>
2021-04-11 18:19:04 -0400

    Update docs and verb slider report component

--- src/app/components/verb-slider-report/verb-slider-report.component.ts
+++ src/app/components/verb-slider-report/verb-slider-report.component.ts
@@ -15,7 +15,7 @@ export class VerbSliderReportComponent implements OnInit {
     const report: any = document.querySelector( '.report' );
 
     if(longOverlay) {
-      const reportHeightNumber: number = report.scrollHeight;
+      const reportHeightNumber: number = report.offsetHeight;
       const reportHeightString: string = reportHeightNumber.toString() + 'px';
       console.log(reportHeightString);
       longOverlay.style.height = reportHeightString;
